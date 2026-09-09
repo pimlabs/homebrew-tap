b81d90c488c1510a2f21e06518a4bcf668f892af
Eko Dedy <eko.dedy.purnomo@gmail.com>
2026-09-09 03:23:16 +0000

    brewmaster 0.12.1

--- Formula/brewmaster.rb
+++ Formula/brewmaster.rb
@@ -1,9 +1,9 @@
 class Brewmaster < Formula
   desc "Selective Homebrew package upgrades by semver bump level"
   homepage "https://github.com/pimlabs/brewmaster"
-  url "https://github.com/pimlabs/brewmaster/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "a641e141e174d941647fcca949ba158c969e78ba609c31c57f9a6436a0e28f02"
-  version "0.12.0"
+  url "https://github.com/pimlabs/brewmaster/archive/refs/tags/v0.12.1.tar.gz"
+  sha256 "6b7999ef922f2158c6f8f17eae8786a8671c9b6b8cbac038ea446883e48938a7"
+  version "0.12.1"
   license "MIT"
 
   depends_on "jq"
@@ -23,6 +23,6 @@ class Brewmaster < Formula
   end
 
   test do
-    assert_match "brewmaster 0.12.0", shell_output("#{bin}/brewmaster --version")
+    assert_match "brewmaster 0.12.1", shell_output("#{bin}/brewmaster --version")
   end
 end
